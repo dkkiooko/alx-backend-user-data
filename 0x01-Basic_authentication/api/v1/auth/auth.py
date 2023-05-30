8ab824a46e98792e8390e27b8624169768e35dcd
dkkiooko <kithekadankioko@gmail.com>
2023-05-30 11:41:20 +0300

    basic authentication

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+""" manage API authentication
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """ class manages API authentication """
+    def __init__(self):
+        """ initialize Auth """
+        pass
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ find whether path is authorized """
+        if path is None or excluded_paths is None or not excluded_paths:
+            return True
+        if path[-1] != '/':
+            path = path + '/'
+        if path in excluded_paths:
+            return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """ find header based on Flask request object """
+        if request is None:
+            return None
+        return request.headers.get("Authorization", None)
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ find the current user """
+        return None
