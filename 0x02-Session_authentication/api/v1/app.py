5280ed4bd283818f724bb0561054a0aef057e9cf
dkkiooko <kithekadankioko@gmail.com>
2023-06-02 19:38:06 +0300

    require authorization whe using session_auth

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -57,8 +57,15 @@ def before_request() -> str:
     excluded_paths = [
         '/api/v1/status/',
         '/api/v1/unauthorized/',
-        '/api/v1/forbidden/'
+        '/api/v1/forbidden/',
+        '/api/v1/auth_session/login/'
     ]
+
+    if auth.authorization_header(request):
+        return None, abort(401)
+
+    if auth.session_cookie(request):
+        return None, abort(401)
 
     if not auth.require_auth(request.path, excluded_paths):
         return
