d208140591c1699c3e7fc6340fd13c22e6c03abd
dkkiooko <kithekadankioko@gmail.com>
2023-06-03 14:00:22 +0300

    created a view "session_auth" that has an authentication system based on a Session ID stored in cookie

---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+""" view for session authentication
+"""
+from api.v1.views import app_views
+from flask import abort, jsonify, request
+from models.user import User
+from os import getenv
+
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def login():
+    """route for /api/v1/auth_session/login
+    captures all credentials for session authentication
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if not email:
+        return jsonify({"error": "email missing"}), 400
+    if not password:
+        return jsonify({"error": "password missing"}), 400
+    found_users = User.search({'email': email})
+    if not found_users:
+        return jsonify({"error": "no user found for this email"}), 404
+
+    for user in found_users:
+        if not user.is_valid_password(password):
+            return jsonify({"error": "wrong password"}), 404
+    found_user = found_users[0]
+
+    from api.v1.app import auth
+    session_id = auth.create_session(found_user.id)
+    SESSION_COOKIE = getenv('SESSION_NAME')
+
+    response = jsonify(found_user.to_json())
+    response.set_cookie(SESSION_COOKIE, session_id)
+    return response
