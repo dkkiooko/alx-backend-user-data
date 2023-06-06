3c46744adf4764ee74eecfaaf98ba834c87eae9f
dkkiooko <kithekadankioko@gmail.com>
2023-06-06 11:18:17 +0300

    created a model named user that creates a table called users with id a primary key, email, hashed_password all non nullable and session_id and reset_token nullable strings

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""SQLAlchemy model for database table users
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class User(Base):
+    """create a model database
+
+    Args:
+        Base (_sqlalchemy_): _class with directives to describe table in sql_
+    """
+    __tablename__ = 'users'
+
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
