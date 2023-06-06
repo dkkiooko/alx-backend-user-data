e405a29ddf4c8e50c80e0eff321e5772f0c9e340
dkkiooko <kithekadankioko@gmail.com>
2023-06-06 15:47:15 +0300

    added a try except block to capture any exceptions

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -39,7 +39,11 @@ class DB:
             email (str): _email of new user_
             hashed_password (str): _password associated with username_
         """
-        new_user = User(email=email, hashed_password=hashed_password)
-        self._session.add(new_user)
-        self._session.commit()
+        try:
+            new_user = User(email=email, hashed_password=hashed_password)
+            self._session.add(new_user)
+            self._session.commit()
+        except Exception:
+            self._session.rollback()
+            new_user = None
         return new_user
