8a9f5d49a3ea057cd44a0f33f4b91dd9178275e7
dkkiooko <kithekadankioko@gmail.com>
2023-06-06 17:58:40 +0300

    Auth.register_user creates a new user in the database. If email already exists in a database then ValueError is raised

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,6 +2,9 @@
 """hash password
 """
 import bcrypt
+from db import DB
+from sqlalchemy.orm.exc import NoResultFound
+from user import User
 
 
 def _hash_password(password: str) -> bytes:
@@ -16,3 +19,23 @@ def _hash_password(password: str) -> bytes:
     pwd = password.encode('utf-8')
     salt = bcrypt.gensalt()
     return bcrypt.hashpw(pwd, salt)
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """ register new user if user already exists raise an error
+        """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            pass
+        pwd = _hash_password(password=password)
+        user = self._db.add_user(email=email, hashed_password=pwd)
+        return user
