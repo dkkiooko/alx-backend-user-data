00c014fea8999fe3b7057df53ae8cb191b79811d
dkkiooko <kithekadankioko@gmail.com>
2023-06-08 19:44:50 +0300

    used a try except block when finding user by email. Used the update user function to update the reset token uuid'

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -100,9 +100,10 @@ class Auth:
 
     def get_reset_password_token(self, email: str) -> str:
         """ generates reset password token """
-        user = self._db.find_user_by(email=email)
-        if user is None:
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
             raise ValueError
         reset_token = _generate_uuid()
-        user.reset_token = reset_token
+        self._db.update_user(user.id, reset_token=reset_token)
         return reset_token
