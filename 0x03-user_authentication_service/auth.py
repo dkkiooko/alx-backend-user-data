a30b906f2d3005bf5ad7e82092c907025dd684d8
dkkiooko <kithekadankioko@gmail.com>
2023-06-08 19:38:34 +0300

    Auth.get_reset_password_token takes an email and returns a string that is a reset_token which is a UUID

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -97,3 +97,12 @@ class Auth:
             return None
         except Exception:
             return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """ generates reset password token """
+        user = self._db.find_user_by(email=email)
+        if user is None:
+            raise ValueError
+        reset_token = _generate_uuid()
+        user.reset_token = reset_token
+        return reset_token
